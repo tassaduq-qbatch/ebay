3948e9f6dc71e5790d0682aca2b6701bf8f722df
Apple <apple@Apples-iMac.local>
2019-08-16 15:50:48 +0500

    Add force fetch restriction

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,5 +2,6 @@ pages <<{
   page_type: 'listings',
   method: 'GET',
   url: 'https://www.ebay.com/b/Apple-iPhone/9355/bn_319682',
+  freshness: "2019-08-15T13:59:29.91741Z",
   force_fetch: true
 }
